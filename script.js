292ebdbdcf41978b9d581d8d2c50581a5ca4dce8
Martin Zuvilivia <88513813+moncho100@users.noreply.github.com>
2022-06-30 11:15:40 -0300

    Agregando comentarios

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ resultado = numero * i;
 //Ejemplo FOR - BREAK
 
 for(let m = 2; m <= 20; m++){
+    //una vez que llegue a la repeticion 15 corta el ciclo ahi, antes de la 16
     if(m == 16){
         break;
     }
@@ -19,6 +20,7 @@ for(let m = 2; m <= 20; m++){
 //Ejemplo FOR - CONTINUE
 
 for(let f = 1; f <= 17; f++){
+    //una vez que llegue a la repeticion 9 que la siguiente sea la 11, es decir saltea la repeticion 10
     if(f == 10){
         continue;
     }
